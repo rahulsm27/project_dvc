3b7aaff7d3a11dc72b5f0a1bd11bdebe0e061f04
Rahul <rahulsm.27@gmail.com>
2023-11-28 11:29:24 +0530

    Added functinality to initialize DVC

--- src/version_data.py
+++ src/version_data.py
@@ -1,10 +1,11 @@
 from src.config_schemas.config_schema import Config
 from src.utils.config_utils import get_config
+from src.utils.data_utils import initialize_dvc
 
 # custom decorate created
 @get_config(config_path="../configs", config_name="config")
 def version_data(config: Config) -> None:
-    print(config)
+    initialize_dvc()
 
 
 if __name__ == "__main__":
